10e60c98e0283918928a611ae808e7bb9cffd990
george <xujj86@126.com>
2020-11-09 17:25:46 +0800

    增加日志功能

--- src/main/java/com/xgit/devops/devopssample/controller/HelloWorldController.java
+++ src/main/java/com/xgit/devops/devopssample/controller/HelloWorldController.java
@@ -1,13 +1,15 @@
 package com.xgit.devops.devopssample.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Slf4j
 public class HelloWorldController {
     @RequestMapping("/hanyun")
     public String sayHello() {
-        System.out.println("#####################################  /hanyun/api/v1/getname");
+        log.info("#####################################  /hanyun/api/v1/getname");
         return "#####################################  /hanyun/api/v1/getname.";
     }
 }
